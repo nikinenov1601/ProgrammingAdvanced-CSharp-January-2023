830f0d54ecab753d553c653de914a7b3e42a90ba
nikinenov1601 <nikinenov1@abv.bg>
2024-02-16 21:19:21 +0200

    Adding eight problem to Stacks and Queues - Lab

---
+++ 08.TrafficJam/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        int capacity = int.Parse(Console.ReadLine());
+
+        Queue<string> queue = new Queue<string>();
+        int passedCars = 0;
+
+        while (true)
+        {
+            string command = Console.ReadLine();
+
+            if (command == "end")
+            {
+                Console.WriteLine($"{passedCars} cars passed the crossroads.");
+                break;
+            }
+
+            if (command == "green")
+            {
+                int carsToPass = Math.Min(queue.Count, capacity);
+                for (int i = 0; i < carsToPass; i++)
+                {
+                    string car = queue.Dequeue();
+                    Console.WriteLine($"{car} passed!");
+                    passedCars++;
+                }
+            }
+            else
+            {
+                queue.Enqueue(command);
+            }
+        }
+    }
+}
