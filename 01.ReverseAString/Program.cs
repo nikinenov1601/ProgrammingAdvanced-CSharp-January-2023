3c90b51eb181eecf00db663d97e0f59e6f312c91
nikinenov1601 <nikinenov1@abv.bg>
2024-02-16 21:15:56 +0200

    Adding first problem to Stacks and Queues - Lab

---
+++ 01.ReverseAString/Program.cs
@@ -0,0 +1,12 @@
+﻿var input = Console.ReadLine();
+var stack = new Stack<char>();
+
+foreach (var letter in input)
+{
+    stack.Push(letter);
+}
+
+while (stack.Count > 0)
+{
+    Console.Write(stack.Pop());
+}
