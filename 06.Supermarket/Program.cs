8b735a1339fce826ac46e0d0a85c7f3fe4371730
nikinenov1601 <nikinenov1@abv.bg>
2024-02-16 21:18:38 +0200

    Adding sixth problem to Stacks and Queues - Lab

---
+++ 06.Supermarket/Program.cs
@@ -0,0 +1,26 @@
+﻿
+Queue<string> queue = new Queue<string>();
+
+while (true)
+{
+    string input = Console.ReadLine();
+
+    if (input == "End")
+    {
+        Console.WriteLine($"{queue.Count} people remaining.");
+        break;
+    }
+
+    else if (input == "Paid")
+    {
+        while (queue.Count > 0)
+        {
+            Console.WriteLine(queue.Dequeue());
+        }
+    }
+
+    else
+    {
+        queue.Enqueue(input);
+    }
+}
