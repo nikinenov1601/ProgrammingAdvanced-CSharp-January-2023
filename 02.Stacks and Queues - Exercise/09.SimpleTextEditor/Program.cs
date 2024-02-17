4de201aca8b629aa0b3b218e60fb9bec6a1b5163
nikinenov1601 <nikinenov1@abv.bg>
2024-02-17 22:47:56 +0200

    Adding ninth problem solution to Stacks and Queues - Exercise

---
+++ 02.Stacks and Queues - Exercise/09.SimpleTextEditor/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+
+int n = int.Parse(Console.ReadLine());
+Stack<string> history = new Stack<string>();
+StringBuilder text = new StringBuilder();
+
+for (int i = 0; i < n; i++)
+{
+    string[] operation = Console.ReadLine().Split();
+    string command = operation[0];
+
+    if (command == "1")
+    {
+        string argument = operation[1];
+        history.Push(text.ToString());
+        text.Append(argument);
+    }
+    else if (command == "2")
+    {
+        int count = int.Parse(operation[1]);
+        history.Push(text.ToString());
+        text.Length -= count;
+    }
+    else if (command == "3")
+    {
+        int index = int.Parse(operation[1]) - 1;
+        Console.WriteLine(text[index]);
+    }
+    else if (command == "4")
+    {
+        text = new StringBuilder(history.Pop());
+    }
+}
