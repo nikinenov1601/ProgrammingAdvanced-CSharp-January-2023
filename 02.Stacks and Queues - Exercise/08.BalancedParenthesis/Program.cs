45b36150dab28f41cb100eda92425a14e22fbdc1
nikinenov1601 <nikinenov1@abv.bg>
2024-02-17 22:47:38 +0200

    Adding eight problem solution to Stacks and Queues - Exercise

---
+++ 02.Stacks and Queues - Exercise/08.BalancedParenthesis/Program.cs
@@ -0,0 +1,37 @@
+﻿string sequence = Console.ReadLine();
+Stack<char> stack = new Stack<char>();
+
+foreach (char c in sequence)
+{
+    if (c == '(' || c == '{' || c == '[')
+    {
+        stack.Push(c);
+    }
+    else
+    {
+        if (stack.Count == 0)
+        {
+            Console.WriteLine("NO");
+            return;
+        }
+
+        char top = stack.Pop();
+
+        if ((c == ')' && top != '(') ||
+            (c == '}' && top != '{') ||
+            (c == ']' && top != '['))
+        {
+            Console.WriteLine("NO");
+            return;
+        }
+    }
+}
+
+if (stack.Count == 0)
+{
+    Console.WriteLine("YES");
+}
+else
+{
+    Console.WriteLine("NO");
+}
