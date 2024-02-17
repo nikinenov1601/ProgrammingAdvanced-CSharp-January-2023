302ab5629eee7e0b3d7882fd967e9d179f2ac85d
nikinenov1601 <nikinenov1@abv.bg>
2024-02-17 22:45:12 +0200

    Adding first problem solution to 01.Stacks and Queues - Exercise

---
+++ 02.Stacks and Queues - Exercise/01.BasicStackOperations/Program.cs
@@ -0,0 +1,47 @@
+﻿
+string[] input = Console.ReadLine().Split();
+
+int N = int.Parse(input[0]);
+int S = int.Parse(input[1]);
+int X = int.Parse(input[2]);
+
+string[] arrayOfNumbers = Console.ReadLine().Split();
+
+Stack<int> stack = new Stack<int>();
+
+for (int i = 0; i < N; i++)
+{
+    stack.Push(int.Parse(arrayOfNumbers[i]));
+}
+
+for (int i = 0; i < S; i++)
+{
+    if (stack.Count > 0)
+    {
+        stack.Pop();
+    }
+}
+
+if (stack.Count == 0)
+{
+    Console.WriteLine(0);
+}
+
+else if (stack.Contains(X))
+{
+    Console.WriteLine("true");
+}
+
+else
+{
+    int minNumber = int.MaxValue;
+
+    foreach (var number in stack)
+    {
+        if (minNumber > number)
+        {
+            minNumber = number;
+        }
+    }
+    Console.WriteLine(minNumber);
+}
