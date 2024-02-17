69193aae5c084c83a5323d4b12df7c26c6dd6619
nikinenov1601 <nikinenov1@abv.bg>
2024-02-17 22:46:43 +0200

    Adding fifth problem solution to Stacks and Queues - Exercise

---
+++ 02.Stacks and Queues - Exercise/05.FashionBoutique/Program.cs
@@ -0,0 +1,30 @@
+﻿
+int[] clothes = Console.ReadLine()
+    .Split()
+    .Reverse()
+    .Select(int.Parse)
+    .ToArray();
+
+Stack<int> stack = new Stack<int>(clothes);
+int capacity = int.Parse(Console.ReadLine());
+int secondValueForCapacity = capacity;
+
+int racksCounter = 1;
+
+while (stack.Count > 0)
+{
+    int cuurentClothing = stack.Peek();
+
+    if (capacity >= cuurentClothing)
+    {
+        stack.Pop();
+        capacity -= cuurentClothing;
+    }
+    else
+    {
+        racksCounter++;
+        capacity = secondValueForCapacity;
+    }
+}
+
+Console.WriteLine(racksCounter);
