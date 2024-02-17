7f82d0b0caa5ecf4d185805f6ce889afb8bd14b5
nikinenov1601 <nikinenov1@abv.bg>
2024-02-17 22:46:25 +0200

    Adding fourth problem solution to Stacks and Queues - Exercise

---
+++ 02.Stacks and Queues - Exercise/04.FastFood/Program.cs
@@ -0,0 +1,38 @@
+﻿
+int food = int.Parse(Console.ReadLine());
+
+int[] orders = Console.ReadLine()
+    .Split()
+    .Reverse()
+    .Select(int.Parse)
+    .ToArray();
+
+Stack<int> stack = new Stack<int>(orders);
+
+Console.WriteLine(stack.Max());
+
+while (stack.Count > 0)
+{
+    int firstNumber = stack.Peek();
+
+    if (food >= firstNumber)
+    {
+        stack.Pop();
+        food -= firstNumber;
+    }
+
+    else
+    {
+        break;
+    }
+}
+
+if (stack.Count == 0)
+{
+    Console.WriteLine("Orders complete");
+}
+
+else
+{
+    Console.Write($"Orders left: " + string.Join(" ", stack));
+}
