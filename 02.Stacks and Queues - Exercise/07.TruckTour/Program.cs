e88432a42ab336a68b06f6f5a889cdfe54cd3ea4
nikinenov1601 <nikinenov1@abv.bg>
2024-02-17 22:47:22 +0200

    Adding seventh problem solution to Stacks and Queues - Exercise

---
+++ 02.Stacks and Queues - Exercise/07.TruckTour/Program.cs
@@ -0,0 +1,26 @@
+﻿int n = int.Parse(Console.ReadLine());
+int[][] petrolPumps = new int[n][];
+
+for (int i = 0; i < n; i++)
+{
+    string[] input = Console.ReadLine().Split();
+    int petrol = int.Parse(input[0]);
+    int distance = int.Parse(input[1]);
+    petrolPumps[i] = new int[] { petrol, distance };
+}
+
+int start = 0;
+int remainingPetrol = 0;
+
+for (int i = 0; i < n; i++)
+{
+    remainingPetrol += petrolPumps[i][0] - petrolPumps[i][1];
+
+    if (remainingPetrol < 0)
+    {
+        start = i + 1;
+        remainingPetrol = 0;
+    }
+}
+
+Console.WriteLine(start);
