e59432a02c79834eaad4bd42906e5906369d4cb0
nikinenov1601 <nikinenov1@abv.bg>
2024-02-19 23:43:54 +0200

    Adding second problem solution to Multidimensional Arrays - Lab

---
+++ 03.Multidimensional Arrays - Lab/02.SumMatrixColumns/Program.cs
@@ -0,0 +1,25 @@
+﻿
+int[] sizes = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+
+int[,] matrix = new int[sizes[0], sizes[1]];
+
+for (int i = 0; i < sizes[0]; i++)
+{
+    int[] rowItems = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+    for (int j = 0; j < sizes[1]; j++)
+    {
+        matrix[i, j] = rowItems[j];
+    }
+}
+
+for (int j = 0; j < sizes[1]; j++)
+{
+    int sumCol = 0;
+
+    for (int i = 0; i < sizes[0]; i++)
+    {
+        sumCol += matrix[i, j];
+    }
+    Console.WriteLine(sumCol);
+}
