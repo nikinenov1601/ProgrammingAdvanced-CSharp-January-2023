ddc3b3ece42d93836a32cfb0de4c50a5d83530b0
nikinenov1601 <nikinenov1@abv.bg>
2024-02-19 23:44:18 +0200

    Adding third problem solution to Multidimensional Arrays - Lab

---
+++ 03.Multidimensional Arrays - Lab/03.PrimaryDiagonal/Program.cs
@@ -0,0 +1,26 @@
+﻿
+int number = int.Parse(Console.ReadLine()); // Един номер за row & col
+
+int[,] matrix = new int[number, number]; // Задаваме размера на матрицата
+
+int sum = 0;
+
+for (int row = 0; row < number; row++)
+{
+    int[] elements = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+    for (int col = 0; col < number; col++)
+    {
+        matrix[row, col] = elements[col]; // Попълваме матрицата
+    }
+}
+
+for (int row = 0; row < number; row++)
+{
+    for (int col = 0; col < number; col++)
+    {
+        sum += matrix[row, col];
+        row++;
+    }
+}
+Console.WriteLine(sum);
