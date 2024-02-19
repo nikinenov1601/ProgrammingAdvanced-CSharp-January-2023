03c7067cdf9d18e5d7a245830b8103fc2e9d99fd
nikinenov1601 <nikinenov1@abv.bg>
2024-02-19 23:44:35 +0200

    Adding fourth problem solution to Multidimensional Arrays - Lab

---
+++ 03.Multidimensional Arrays - Lab/04.SymbolInMatrix/Program.cs
@@ -0,0 +1,43 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+char[,] matrix = new char[n, n];
+
+for (int i = 0; i < n; i++)
+{
+    string row = Console.ReadLine();
+    for (int j = 0; j < n; j++)
+    {
+        matrix[i, j] = row[j];
+    }
+}
+
+char symbol = char.Parse(Console.ReadLine());
+
+bool found = false;
+int symbolRow = -1;
+int symbolCol = -1;
+
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        if (matrix[i, j] == symbol)
+        {
+            symbolRow = i;
+            symbolCol = j;
+            found = true;
+            break;
+        }
+    }
+    if (found)
+        break;
+}
+
+if (found)
+{
+    Console.WriteLine($"({symbolRow}, {symbolCol})");
+}
+else
+{
+    Console.WriteLine($"{symbol} does not occur in the matrix");
+}
