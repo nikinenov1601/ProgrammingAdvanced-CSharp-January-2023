bbd84ffa23e61852ca10b00de8a86342b1ca0b78
nikinenov1601 <nikinenov1@abv.bg>
2024-02-19 23:46:25 +0200

    Adding seventh problem solution to Multidimensional Arrays - Lab

---
+++ 03.Multidimensional Arrays - Lab/07.PascalTriangle/Program.cs
@@ -0,0 +1,30 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+int[][] pascalsTriangle = new int[n][];
+
+for (int i = 0; i < n; i++)
+{
+    pascalsTriangle[i] = new int[i + 1];
+    pascalsTriangle[i][0] = 1; // First element of each row is always 1
+
+    for (int j = 1; j < i; j++)
+    {
+        pascalsTriangle[i][j] = pascalsTriangle[i - 1][j - 1] + pascalsTriangle[i - 1][j];
+    }
+
+    pascalsTriangle[i][i] = 1; // Last element of each row is always 1
+}
+
+// Print Pascal's triangle
+for (int i = 0; i < n; i++)
+{
+    // Add leading spaces for formatting
+    Console.Write(new string(' ', (n - i - 1) * 2));
+
+    for (int j = 0; j <= i; j++)
+    {
+        Console.Write(pascalsTriangle[i][j] + " ");
+    }
+
+    Console.WriteLine();
+}
