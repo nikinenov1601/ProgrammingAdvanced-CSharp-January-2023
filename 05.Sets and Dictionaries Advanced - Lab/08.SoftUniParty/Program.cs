e03a4203da20c57db5383ece8aac9e336e04f06e
nikinenov1601 <nikinenov1@abv.bg>
2024-02-24 12:26:41 +0200

    Adding eight problem solution to Sets and Dictionaries Advanced - Lab

---
+++ 05.Sets and Dictionaries Advanced - Lab/08.SoftUniParty/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualBasic;
+
+string input = Console.ReadLine();
+HashSet<string> reservationNumbers = new HashSet<string>();
+
+while (input != "PARTY")
+{
+    reservationNumbers.Add(input);
+
+    input = Console.ReadLine();
+}
+
+while (input != "END")
+{
+    if (reservationNumbers.Contains(input))
+    {
+        reservationNumbers.Remove(input);
+    }
+
+    input = Console.ReadLine();
+}
+
+Console.WriteLine(reservationNumbers.Count);
+
+var sortedStrings = reservationNumbers.OrderBy(s => !char.IsDigit(s.FirstOrDefault()));
+
+foreach (string reservationNumber in sortedStrings)
+{
+    Console.WriteLine(reservationNumber);
+}
