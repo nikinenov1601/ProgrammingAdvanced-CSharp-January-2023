bcc2d57c872da95d5c9401e22149019906ff108b
nikinenov1601 <nikinenov1@abv.bg>
2024-02-24 12:25:46 +0200

    Adding fifth problem solution to Sets and Dictionaries Advanced - Lab

---
+++ 05.Sets and Dictionaries Advanced - Lab/05.CitiesByContinentAndCountry/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int numEntries = int.Parse(Console.ReadLine().Trim());
+
+        Dictionary<string, Dictionary<string, HashSet<string>>> cityDictionary = new Dictionary<string, Dictionary<string, HashSet<string>>>();
+
+        for (int i = 0; i < numEntries; i++)
+        {
+            string[] input = Console.ReadLine().Trim().Split();
+            string continent = input[0];
+            string country = input[1];
+            string city = input[2];
+
+            if (!cityDictionary.ContainsKey(continent))
+            {
+                cityDictionary[continent] = new Dictionary<string, HashSet<string>>();
+            }
+
+            if (!cityDictionary[continent].ContainsKey(country))
+            {
+                cityDictionary[continent][country] = new HashSet<string>();
+            }
+
+            cityDictionary[continent][country].Add(city);
+        }
+
+        PrintCityDictionary(cityDictionary);
+    }
+
+    static void PrintCityDictionary(Dictionary<string, Dictionary<string, HashSet<string>>> cityDictionary)
+    {
+        foreach (var continentCountries in cityDictionary)
+        {
+            Console.WriteLine($"{continentCountries.Key}:");
+            foreach (var countryCities in continentCountries.Value)
+            {
+                Console.WriteLine($"  {countryCities.Key} -> {string.Join(", ", countryCities.Value)}");
+            }
+        }
+    }
+}
