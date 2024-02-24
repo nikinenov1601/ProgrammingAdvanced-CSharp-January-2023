9f670d9292872f6421f1a67856a73bdcc09fd3f1
nikinenov1601 <nikinenov1@abv.bg>
2024-02-24 12:24:18 +0200

    Adding first problem solution to Sets and Dictionaries Advanced - Lab

---
+++ 05.Sets and Dictionaries Advanced - Lab/01.CountSameValuesInArray/Program.cs
@@ -0,0 +1,28 @@
+﻿
+List<double> numbers = Console.ReadLine().
+    Split(" ", StringSplitOptions.RemoveEmptyEntries).
+    Select(double.Parse).
+    ToList();
+
+// Правим си лист за да определим стойностите, които ще използваме
+
+Dictionary<double, int> numbersCount = new Dictionary<double, int>();
+
+// Въвеждаме един празен речник в който ще попълваме ключ и стойност
+
+foreach (double number in numbers) // за всяко число от листа
+{
+    if (!numbersCount.ContainsKey(number)) // Ако няма такъв ключ в речника
+    {
+        numbersCount.Add(number, 1); // Се въвежда със стойност 1
+    }
+    else // Ако има такъв ключ в речника
+    {
+        numbersCount[number]++; // Увеличаваме стойноста му с 1
+    }
+}
+
+foreach (var count in numbersCount) // За всеки един ключ в речника
+{
+    Console.WriteLine($"{count.Key} - {count.Value} times");
+}
