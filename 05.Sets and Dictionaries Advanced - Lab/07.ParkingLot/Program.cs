d1b66114d8b304eafffe79ebc30c0a9468e31afd
nikinenov1601 <nikinenov1@abv.bg>
2024-02-24 12:26:24 +0200

    Adding seventh problem solution to Sets and Dictionaries Advanced - Lab

---
+++ 05.Sets and Dictionaries Advanced - Lab/07.ParkingLot/Program.cs
@@ -0,0 +1,39 @@
+﻿
+string input = Console.ReadLine();
+
+HashSet<string> carNumbers = new HashSet<string>();
+
+while (input != "END")
+{
+    string[] splittedInput = input.Split(", ");
+
+    string command = splittedInput[0];
+    string number = splittedInput[1];
+
+    if (command == "IN")
+    {
+        carNumbers.Add(number);
+    }
+
+    else if (command == "OUT")
+    {
+        carNumbers.Remove(number);
+    }
+
+
+    input = Console.ReadLine();
+
+}
+
+if (carNumbers.Count == 0)
+{
+    Console.WriteLine("Parking Lot is Empty");
+}
+
+else
+{
+    foreach (string car in carNumbers)
+    {
+        Console.WriteLine(car);
+    }
+}
