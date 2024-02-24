47b9f2943ac391603c410988649317071a79f8ed
nikinenov1601 <nikinenov1@abv.bg>
2024-02-24 12:25:06 +0200

    Adding third problem solution to Sets and Dictionaries Advanced - Lab

---
+++ 05.Sets and Dictionaries Advanced - Lab/03.Largest3Numbers/Program.cs
@@ -0,0 +1,3 @@
+﻿List<int> numbers = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).OrderByDescending(x => x).Take(3).ToList();
+
+Console.WriteLine(string.Join(" ", numbers));
