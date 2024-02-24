83ff53b26e7531ad475b10ab897decd8c87b169e
nikinenov1601 <nikinenov1@abv.bg>
2024-02-24 12:24:48 +0200

    Adding second problem solution to Sets and Dictionaries Advanced - Lab

---
+++ 05.Sets and Dictionaries Advanced - Lab/02.AverageStudentGrades/Program.cs
@@ -0,0 +1,28 @@
+﻿
+int n = int.Parse(Console.ReadLine());
+var grades = new Dictionary<string, List<decimal>>();
+
+for (int i = 0; i < n; i++)
+{
+    var tokens = Console.ReadLine().Split();
+    var name = tokens[0];
+    var grade = decimal.Parse(tokens[1]);
+
+    if (!grades.ContainsKey(name))
+    {
+        grades[name] = new List<decimal>();
+    }
+    grades[name].Add(grade);
+
+}
+foreach (var (name, studentGrades) in grades)
+{
+    var average = studentGrades.Average();
+    Console.Write($"{name} -> ");
+
+    foreach (var grade in studentGrades)
+    {
+        Console.Write($"{grade:f2} ");
+    }
+    Console.WriteLine($"(avg: {average:f2})");
+}
