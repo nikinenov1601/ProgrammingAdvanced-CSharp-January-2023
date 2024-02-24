849c7334c2b4e5c441f988d5e89fac5f5462250b
nikinenov1601 <nikinenov1@abv.bg>
2024-02-24 12:26:06 +0200

    Adding sixth problem solution to Sets and Dictionaries Advanced - Lab

---
+++ 05.Sets and Dictionaries Advanced - Lab/06.RecordUniqueNames/Program.cs
@@ -0,0 +1,20 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+HashSet<string> names = new HashSet<string>();
+
+for (int i = 0; i < n; i++)
+{
+    string name = Console.ReadLine();
+
+    if (names.Contains(name))
+    {
+        names.Remove(name);
+    }
+
+    names.Add(name);
+}
+
+foreach (string name in names)
+{
+    Console.WriteLine(name);
+}
