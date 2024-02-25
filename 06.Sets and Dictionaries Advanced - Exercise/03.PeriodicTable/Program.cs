8ee8c543d7e66dd532eecb6ed5b59bd42c412696
nikinenov1601 <nikinenov1@abv.bg>
2024-02-25 13:45:35 +0200

    Adding third problem solution to 06.Sets and Dictionaries Advanced - Exercise

---
+++ 06.Sets and Dictionaries Advanced - Exercise/03.PeriodicTable/Program.cs
@@ -0,0 +1,15 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+SortedSet<string> uniqueElements = new SortedSet<string>();
+
+for (int i = 0; i < n; i++)
+{
+    string[] compounds = Console.ReadLine().Split(' ');
+
+    foreach (string compound in compounds)
+    {
+        uniqueElements.Add(compound);
+    }
+}
+
+Console.WriteLine(string.Join(" ", uniqueElements));
