408502ac782f22e90aa169d471041a565b01fbde
nikinenov1601 <nikinenov1@abv.bg>
2024-02-25 13:45:51 +0200

    Adding fourth problem solution to 06.Sets and Dictionaries Advanced - Exercise

---
+++ 06.Sets and Dictionaries Advanced - Exercise/04.EvenTimes/Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+Dictionary<int, int> numbersCount = new();
+
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < n; i++)
+{
+    int number = int.Parse(Console.ReadLine());
+
+    if (!numbersCount.ContainsKey(number))
+    {
+        numbersCount.Add(number, 0);
+    }
+
+    numbersCount[number]++;
+}
+
+int result = numbersCount.Single(nc => nc.Value % 2 == 0).Key;
+
+Console.WriteLine(result);
