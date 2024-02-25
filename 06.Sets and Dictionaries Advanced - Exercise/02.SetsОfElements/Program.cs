6d98f90815f2ebfaf759d4285ed49362eaaf1a1a
nikinenov1601 <nikinenov1@abv.bg>
2024-02-25 13:45:16 +0200

    Adding second problem solution to 06.Sets and Dictionaries Advanced - Exercise

---
+++ 06.Sets and Dictionaries Advanced - Exercise/02.SetsОfElements/Program.cs
@@ -0,0 +1,25 @@
+﻿
+HashSet<int> firstSet = new();
+
+HashSet<int> secondSet = new();
+
+HashSet<int> thirdSet = new();
+
+int[] count = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToArray();
+
+for (int i = 0; i < count[0]; i++)
+{
+    firstSet.Add(int.Parse(Console.ReadLine()));
+}
+
+for (int i = 0; i < count[1]; i++)
+{
+    secondSet.Add(int.Parse(Console.ReadLine()));
+}
+
+firstSet.IntersectWith(secondSet);
+
+Console.WriteLine(String.Join(" ", firstSet));
