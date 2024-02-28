0569d2035af2e9732e875caaa9e9fa8d8a70d4e2
nikinenov1601 <nikinenov1@abv.bg>
2024-02-28 17:39:43 +0200

    Adding first problem solution to Functional Programming - Lab

---
+++ 09.FunctionalProgramming/01.SortEvenNumbers/Program.cs
@@ -0,0 +1,26 @@
+﻿
+
+string[] strings = new string[] { "1", "2", "3" };
+
+int[] sortedNumbers = Console.ReadLine()
+    .Split(", ")
+    .Select(int.Parse)
+    .Where((int n) =>
+    {
+        return n % 2 == 0;
+    })
+    .OrderBy(mojevsichko => mojevsichko)
+    .ToArray();
+
+Console.WriteLine(String.Join(", ", sortedNumbers));
+
+
+int OrderByNumber(int n)
+{
+    return n;
+}
+
+int ParseString(string input)
+{
+    return int.Parse(input);
+}
